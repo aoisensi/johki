37044fa1e00df6bfaa375a1f4711b3e6c62ba4c7
aoisensi <aoisensi@outlook.jp>
2020-03-02 15:23:13 +0900

    id

---
+++ lib/johki/id.rb
@@ -0,0 +1,11 @@
+module Johki
+  ID_MAGIC_NUMBER = 76561197960265728.freeze
+
+  def account_id_to_steam_id(account_id)
+    account_id + ID_MAGIC_NUMBER
+  end
+
+  def steam_id_to_account_id(steam_id)
+    steam_id - ID_MAGIC_NUMBER
+  end
+end
